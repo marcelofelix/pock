e5844aec4ccf844e3b16d67f44ef2a5adba12ddc
Marcelo Felix <olivera.felix@gmail.com>
2016-04-07 00:00:22 -0300

    Label to controller

---
+++ app/assets/javascripts/product/product-list.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('pockApp').
+  controller('ProductListController', function(products, $location, Menu){
+    Menu.action = 'Produtos';
+    var self = this;
+    self.products = products;
+    self.edit = function(p){
+      $location.path('product/' + p.ean);
+    };
+
+    self.search = function(query){
+      return _.filter(self.products, function(e){
+        return angular.lowercase(e.name).indexOf(angular.lowercase(query)) > -1;
+      });
+    };
+
+    return self;
+  });
