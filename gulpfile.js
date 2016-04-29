a4d5e3ceb090eb2bdc90c41a696eacc87ae0f0fe
Marcelo Felix <olivera.felix@gmail.com>
2016-04-28 23:53:31 -0300

    final config

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,7 @@ gulp.task('watch', ['style', 'js', 'images', 'root'], () => {
   gulp.watch(src.root, ['root']).on('change', livereload.changed);
 })
 
-gulp.task('default', ['clean'], () => {
+gulp.task('heroku:production', ['clean'], () => {
   build = true;
   run(['style', 'js', 'images', 'root'])
 })
